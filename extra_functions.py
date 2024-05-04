3a746cdbcc608ee4a9cb0e98e59bef2458b8e06c
iard01 <ianscottard@gmail.com>
2024-05-03 21:01:15 -0400

    Removed some files, cleaned up functionality

--- extra_functions.py
+++ extra_functions.py
@@ -3,12 +3,10 @@ import re
 def password_valid(passkey):
     chr_list = [r'[A-Z]', r'[a-z]', r'\d', r'[!@#$%^&*()-+]']   
     if not(len(passkey) >= 12):
-        print("error: too short")
         return False
     else:
         for text_chunk in chr_list:
             if re.search(text_chunk, passkey) is None:
-                print("error: missing character")
                 return False
     return True
 
